31dcad680e94c43c78458ad242a44ce37cc73757
MianMianDi <993660980@qq.com>
2020-11-05 14:20:43 +0800

    compute matrix

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -76,7 +76,7 @@ class KNearestNeighbor(object):
                 # not use a loop over dimension, nor use np.linalg.norm().          #
                 #####################################################################
                 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+                dists[i,j]=np.sqrt(np.sum(np.square(X[i]-self.X_train[j])))
                 pass
 
                 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
