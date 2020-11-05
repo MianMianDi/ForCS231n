a4d669ef4ebd1e0d73f019d5fae0c9f7da60e2ee
MianMianDi <993660980@qq.com>
2020-11-05 18:01:42 +0800

    initial version

---
+++ time_function.py
@@ -0,0 +1,18 @@
+def time_function(f, args):
+    """
+    Call a function f with args and return the time (in seconds) that it took to execute.
+    """
+    import time
+    tic = time.time()
+    f(args)
+    toc = time.time()
+    return toc - tic
+
+two_loop_time = time_function(classifier.compute_distances_two_loops, X_test)
+print('Two loop version took %f seconds' % two_loop_time)
+
+one_loop_time = time_function(classifier.compute_distances_one_loop, X_test)
+print('One loop version took %f seconds' % one_loop_time)
+
+no_loop_time = time_function(classifier.compute_distances_no_loops, X_test)
+print('No loop version took %f seconds' % no_loop_time)
