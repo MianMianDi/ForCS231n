a655cb262b8344ca9c226e837b9e353abaf7a4bc
MianMianDi <993660980@qq.com>
2020-11-12 19:47:55 +0800

    complete version:

---
+++ run_batchsize_experiment.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def run_batchsize_experiments(normalization_mode):
+    np.random.seed(231)
+    # Try training a very deep net with batchnorm
+    hidden_dims = [100, 100, 100, 100, 100]
+    num_train = 1000
+    small_data = {
+      'X_train': data['X_train'][:num_train],
+      'y_train': data['y_train'][:num_train],
+      'X_val': data['X_val'],
+      'y_val': data['y_val'],
+    }
+    n_epochs=10
+    weight_scale = 2e-2
+    batch_sizes = [5,10,50]
+    lr = 10**(-3.5)
+    solver_bsize = batch_sizes[0]
+
+    print('No normalization: batch size = ',solver_bsize)
+    model = FullyConnectedNet(hidden_dims, weight_scale=weight_scale, normalization=None)
+    solver = Solver(model, small_data,
+                    num_epochs=n_epochs, batch_size=solver_bsize,
+                    update_rule='adam',
+                    optim_config={
+                      'learning_rate': lr,
+                    },
+                    verbose=False)
+    solver.train()
+    
+    bn_solvers = []
+    for i in range(len(batch_sizes)):
+        b_size=batch_sizes[i]
+        print('Normalization: batch size = ',b_size)
+        bn_model = FullyConnectedNet(hidden_dims, weight_scale=weight_scale, normalization=normalization_mode)
+        bn_solver = Solver(bn_model, small_data,
+                        num_epochs=n_epochs, batch_size=b_size,
+                        update_rule='adam',
+                        optim_config={
+                          'learning_rate': lr,
+                        },
+                        verbose=False)
+        bn_solver.train()
+        bn_solvers.append(bn_solver)
+        
+    return bn_solvers, solver, batch_sizes
+
+batch_sizes = [5,10,50]
+bn_solvers_bsize, solver_bsize, batch_sizes = run_batchsize_experiments('batchnorm')
